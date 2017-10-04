59df541bd14c1abe284ac5b5b1160d82bd1c31d3
Marlon O'Neil <hello@marlononeil.com>
2017-10-04 17:04:19 -0400

    Completed 'Adding cats to the DOM'

--- cats.js
+++ cats.js
@@ -1,11 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   var summonCats = document.querySelector('.summon-cats');
+  var catBox = document.querySelectorAll('.cat-box')
 
   summonCats.addEventListener('click', function(){
-    console.log('test')
+
+    var response = $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      dataType: 'JSON',
+    }).done(function(){
+      console.log(response.responseText);
+
+      var imgTag = document.createElement('img');
+      imgTag.src = response.responseJSON['cats'][0].photo;
+      imgTag.alt = response.responseJSON['cats'][0].name;
+
+      count = 1;
+      var num = Math.floor(Math.random() * 3);
+      console.log(num)
+      catBox[num].appendChild(imgTag);
+    });
+
   });
-
-
 
 });
