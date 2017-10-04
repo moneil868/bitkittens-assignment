159f909020ebcf04f7000f7cdf0e4ac6674dc0dc
Marlon O'Neil <hello@marlononeil.com>
2017-10-04 16:18:30 -0400

    Completed 'Setting up the event handler'

---
+++ cats.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  var summonCats = document.querySelector('.summon-cats');
+
+  summonCats.addEventListener('click', function(){
+    console.log('test')
+  });
+
+
+
+});
